d8d54435cb4a64d83e6076e73d465a69c758c376
AndreTorneiro6 <andre.torneiro12@gmai.com>
2024-04-30 12:24:04 +0100

    add resources folders

---
+++ Android_App/app/src/main/java/com/tidycity/code/views/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.tidycity.code.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tidycity.code.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
