3080525fefea861268721663b518427a566ffbcc
AndreTorneiro6 <andre.torneiro12@gmai.com>
2024-05-03 15:47:08 +0100

    add views file

---
+++ Android_App/app/src/main/java/com/tidycity/code/views/PodAccessActivity.kt
@@ -0,0 +1,15 @@
+package com.tidycity.code.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.tidycity.code.R
+
+class PodAccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pod_access)
+    }
+}
